1e9aa5b3903d849b705c68ccfa4f10d72f89c3ae
jirayut <jirayutmaxza@gmail.com>
2017-06-28 10:09:01 +0700

    Initial commit

---
+++ app/src/main/java/rmu/it/myhelloandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package rmu.it.myhelloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
